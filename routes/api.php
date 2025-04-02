948f48f0fc87162e1660a2be2138b0c942bc47ee
Ali salhab <alisalhab258@gmail.com>
2025-04-02 21:39:07 +0300

    last

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
-// Route::get('/user', function (Request $request) {
-//     return $request;
-// });
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 // Route::post("/tasks",[TaskController::class,"store"]);
 // Route::get("/tasks",[TaskController::class,"index"]);
